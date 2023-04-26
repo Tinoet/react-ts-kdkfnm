83bf61daea736cfafb67e2728af8b8dc97ab6115
Tinoet <elijahtinoet@gmail.com>
2023-04-26 17:11:03 +0100

    1 file modified

--- Image0ne.js
+++ Image0ne.js
@@ -1,7 +1,7 @@
 import { Parallax } from 'react-parallax';
 
 const Container = () => (
-    <Parallax  bgImage="path/to/image.jpg" bgImageAlt="the cat" strength={200}>
+    <Parallax  bgImage="path/to/image.jpg" bgImageAlt="the cat" strength={800}>
         Content goes here. Parallax height grows with content height.
     </Parallax>
 );
