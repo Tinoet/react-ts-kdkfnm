e0f7ddba717bf86a67593065e839816284b32a75
Tinoet <elijahtinoet@gmail.com>
2023-04-27 10:26:42 +0100

    3 files modified, 3 added, 3 deleted

--- App.tsx
+++ App.tsx
@@ -1,8 +1,38 @@
-import * as React from 'react';
-import './style.css';
+import React, { useState } from 'react';
 
-export default function App() {
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+    // Send login request to backend
+  };
+
   return (
-   <h1>hello world</h1>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
   );
-}
+};
+
+export default LoginForm;
--- Image0ne.tsx
+++ Image0ne.tsx
@@ -1,8 +1,8 @@
-import React = require('react');
-import { Parallax } from 'react-parallax';
-
-const Container = () => (
-  <Parallax bgImage="path/to/image.jpg" bgImageAlt="the cat" strength={800}>
-    Content goes here. Parallax height grows with content height.
-  </Parallax>
-);
+// import React = require('react');
+// import { Parallax } from 'react-parallax';
+// import 1 from '../img/1.avif'
+// const ImageOne = () => (
+//   <Parallax bgImage="path/to/image.jpg" bgImageAlt="the cat" strength={800}>
+//     Content goes here. Parallax height grows with content height.
+//   </Parallax>
+// );
